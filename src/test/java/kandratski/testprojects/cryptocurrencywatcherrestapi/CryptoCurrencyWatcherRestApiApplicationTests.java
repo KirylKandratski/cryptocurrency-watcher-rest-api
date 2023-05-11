8f97385f10123e0060a47c6eb42eae485a78e10b
KirylKandratski <kiryl.kandratski@gmail.com>
2023-05-11 17:47:16 +0200

    Initial Commit

---
+++ src/test/java/kandratski/testprojects/cryptocurrencywatcherrestapi/CryptoCurrencyWatcherRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kandratski.testprojects.cryptocurrencywatcherrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCurrencyWatcherRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
